4478cbcd9727626382b13e8def26f966e841a618
Rozco01 <jeffer.rozo.123@gmail.com>
2022-05-04 17:46:48 -0500

    Push antes del abal

--- src/main/java/config/DAOConfig.java
+++ src/main/java/config/DAOConfig.java
@@ -1,5 +1,28 @@
 package config;
 
-public class DAOConfig {
+import static com.mongodb.client.model.Filters.eq;
 
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class DAOConfig {
+	public static void main( String[] args ) {
+
+	    // Replace the uri string with your MongoDB deployment's connection string
+	    String uri = "mongodb+srv://juan:12345@cluster0.ozxhb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+	    try (MongoClient mongoClient = MongoClients.create(uri)) {
+	        MongoDatabase database = mongoClient.getDatabase("db_pacientes");
+	        MongoCollection<Document> collection = database.getCollection("pacientes");
+
+	        Document doc = collection.find(eq("nombre", "Juan")).first();
+	        System.out.println(doc.toJson());
+	    }
+	}
 }
+
+
