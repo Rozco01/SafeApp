4555d3a6631f10dbfa321d148960763909b6e8e0
Rozco01 <jeffer.rozo.123@gmail.com>
2022-05-04 16:20:53 -0500

    Actualizacion antes del maven

---
+++ src/main/java/model/Paciente.java
@@ -0,0 +1,110 @@
+package model;
+
+public class Paciente {
+	
+	//Datos Basicos
+	String nombre;
+	String apellidos;
+	String edad;
+	String direccion;
+	String ciudad;
+	String telefono;
+	
+	//Datos de contacto
+	String nombreContacto;
+	String direccionContacto;
+	String telefonoContacto;
+	String parentesco;
+	
+	//Datos Clinicos
+	String nombreMedico;
+	String diagnostico;
+	String fechaConsulta;
+	String imprecionPersonal;
+	
+	//Getter and Setter
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getEdad() {
+		return edad;
+	}
+	public void setEdad(String edad) {
+		this.edad = edad;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getNombreContacto() {
+		return nombreContacto;
+	}
+	public void setNombreContacto(String nombreContacto) {
+		this.nombreContacto = nombreContacto;
+	}
+	public String getDireccionContacto() {
+		return direccionContacto;
+	}
+	public void setDireccionContacto(String direccionContacto) {
+		this.direccionContacto = direccionContacto;
+	}
+	public String getTelefonoContacto() {
+		return telefonoContacto;
+	}
+	public void setTelefonoContacto(String telefonoContacto) {
+		this.telefonoContacto = telefonoContacto;
+	}
+	public String getParentesco() {
+		return parentesco;
+	}
+	public void setParentesco(String parentesco) {
+		this.parentesco = parentesco;
+	}
+	public String getNombreMedico() {
+		return nombreMedico;
+	}
+	public void setNombreMedico(String nombreMedico) {
+		this.nombreMedico = nombreMedico;
+	}
+	public String getDiagnostico() {
+		return diagnostico;
+	}
+	public void setDiagnostico(String diagnostico) {
+		this.diagnostico = diagnostico;
+	}
+	public String getFechaConsulta() {
+		return fechaConsulta;
+	}
+	public void setFechaConsulta(String fechaConsulta) {
+		this.fechaConsulta = fechaConsulta;
+	}
+	public String getImprecionPersonal() {
+		return imprecionPersonal;
+	}
+	public void setImprecionPersonal(String imprecionPersonal) {
+		this.imprecionPersonal = imprecionPersonal;
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<link href="css/pantalla.css" rel="stylesheet" type="text/css">
+<title>SafeApp</title>
+</head>
+<body>
+ <header>
+        <table class="menu" width="100%" border="0">
+            <tr>
+                <th>
+                    <ul class="lista">
+                        <li><a href="#">RELLENO</a></li>
+                        <li><a href="#">RELLENO</a></li>
+                        <li><a href="#">RELLENO</a></li>
+                        <li><a href="#">RELLENO</a></li>
+                        <li><a href="#">RELLENO</a></li>
+                        <li><a href="#">RELLENO</a></li>
+                        <li><a href="#">RELLENO</a></li>
+                    </ul>
+                </th>
+            </tr>
+        </table>
+    </header>
+</body>
+<div class="fondo">
+ 
+</div>
+<section class="formato">
+    <h3>Crear cuenta</h3>
+    <input class="datosing" type="text" name="usuario" value="" placeholder="usuario">
+    <input class="datosing" type="password" name="contraseña" value="" placeholder="contraseña">
+    <input class="boton"type="button" name="" value="Ingresar">
+    <p><a class="olvidaste" href="#">¿Olvidaste tu contraseña?</a></p>
+</section>
+</html>
